50179aff44a4ddb9849e9ede7aced337e5127809
fariznt <fariz.imran98@gmail.com>
2025-07-14 01:57:24 -0400

    form addition frontend design is layed out; very early stage tho and no api views exist to be called

--- seat_signal/static/seat-signal/seat_signal.js
+++ seat_signal/static/seat-signal/seat_signal.js
@@ -1,36 +1,56 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     console.log('reach')
-//     // Listen for new signal elements to be added
-//     document.querySelector('.add-button').addEventListener('click', add_signal_form);
-
-//     // Load existing signal elements
-//     load_signal_elements();
-// })
-
-// function add_signal_form() {
-//     console.log('reach2')
-//     // deal with later: should have forms in same div as buttons.
-//     document.querySelector('.signal-page').insertAd = `<div class='signal-element'>HELLO HELLO HELLO</div>` 
-//                                                                 + document.querySelector('.signal-page').innerHTML;
-// }
-
-// function load_signal_elements() {
-
-// }
-
 const container = document.querySelector('.signal-page');
-const addBtn    = container.querySelector('.add-button');
-const tpl       = document.getElementById('signal-form-tpl');
+const addBtn = container.querySelector('.add-button');
+const form = document.getElementById('signal-form');
 
 
 document.addEventListener('DOMContentLoaded', () => {
-
-
-    // adding new signal elements
+    load_current()
+    // add form
     addBtn.addEventListener('click', () => add_signal_form());
 });
 
+function load_current() {
+    // use API to get existing signals and load them into the page
+}
+
+
 function add_signal_form() {
-    const form_clone = tpl.content.cloneNode(true);
-    container.insertBefore(form_clone, addBtn);   // keeps button last
+    // remove add button (until submission)
+    addBtn.classList.add('d-none');
+
+    // show new signal form
+    form.classList.remove('d-none');
+
+    // set the form to receive submission
+    const startBtn = container.querySelector('.start');
+    startBtn.addEventListener('click', () => submit_signal_form());
+}
+
+function submit_signal_form() {
+    if (validate_input()) {
+        // post new seat signal
+
+        // remove form used to add signal
+        form.classList.add('d-none');
+
+        // append the completed form
+        temp = document.createElement("span"); // TEMPORARY ELEMENT, WILL BE REPLACED WITH FULL ACTUAL FORM. implementation will be shared with initial completed form loading
+        temp.textContent = 'temp';
+        container.insertBefore(temp, form);
+
+        // bring back add button
+        if (!reached_signal_cap()) {
+            addBtn.classList.remove('d-none')
+        }
+    }
+}
+
+function validate_input() {
+    // TODO code to validate input, if anything bad display message on screen
+    return true; // all valid for now
+}
+
+function reached_signal_cap() {
+    // TODO use api to check if reached
+    return false;
 }
